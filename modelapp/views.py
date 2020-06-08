1abbdb0ff0ed61657e84cc9056d5c69647fc94ea
권은빈 <ds5bean23@gmail.com>
2020-06-08 14:40:15 +0900

    bf style

--- modelapp/views.py
+++ modelapp/views.py
@@ -5,8 +5,5 @@ from .models import Blog
 # Create your views here.
 def home(request):
     article = Article.objects
-    return render(request,'home.html', {'article':article})
-
-def home(request):
-    blog = Article.objects
-    return render(request,'home.html',)
+    blog = Blog.objects
+    return render(request,'home.html', {'article':article, 'blog':blog})
